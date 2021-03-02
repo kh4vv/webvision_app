2009e904f6ae74f539769e7c2e2d4245ac586976
kh4vv <kh4vv@virginia.edu>
2021-03-01 23:40:24 -0600

    pad fix - not finished

--- api/api.py
+++ api/api.py
@@ -5,13 +5,18 @@ from flask_cors import CORS
 from werkzeug.utils import secure_filename
 from PIL import Image
 import numpy as np
-import json 
+import json
+import base64
+import cv2
+import io
 
 import torch
 from inference import mnist_evaluation, quickdraw_evaluation, landmark_evaluation, transform_landmark
 from objdec_inference import yolov3_evaluation
 from model import LeNet, ResNext101Landmark
 
+# Initialize the useless part of the base64 encoded image.
+init_Base64 = 22
 
 app = Flask(__name__)
 CORS(app)
@@ -60,25 +65,33 @@ def mnist_predict():
     if request.method == 'POST':
 
         mnist_draw = request.form['url']
+        print(mnist_draw)
         mnist_draw = mnist_draw[init_Base64:]
+        print(mnist_draw)
         mnist_draw_decoded = base64.b64decode(mnist_draw)
-        print(mnist_draw_decoded)
         # Fix later(to PIL version)
         # Conver bytes array to PIL Image
-        # imageStream = io.BytesIO(draw_decoded)
-        # img = Image.open(imageStream)
-        """
+        imageStream = io.BytesIO(base64.b64decode(mnist_draw))
+        img = Image.open(imageStream)
+        I = np.asarray(img)
+        print(I)
+
         mnist_img = np.asarray(bytearray(mnist_draw_decoded), dtype="uint8")
         mnist_img = cv2.imdecode(mnist_img, cv2.IMREAD_GRAYSCALE)
-        mnist_img = cv2.resize(mnist_img, (28,28), interpolation = cv2.INTER_AREA)
+        mnist_img = cv2.resize(mnist_img, (28, 28),
+                               interpolation=cv2.INTER_AREA)
+        print(mnist_img)
         mnist_img = Image.fromarray(mnist_img)
+        print(mnist_img)
 
-        weight_path = './weight/mnist.pth'
+        weight_path = './weights/mnist.pth'
         mnist_model = LeNet().to(device)
-        mnist_img, mnist_pred = mnist_evaluation(mnist_img, weight_path, mnist_model)
+        mnist_img, mnist_pred = mnist_evaluation(
+            mnist_img, weight_path, mnist_model)
 
         mnist_pred = int(mnist_pred)
-        """
+        print(mnist_pred)
+
         return mnist_draw_decoded
 
     return result
@@ -111,8 +124,39 @@ def landmark_upload_image():
         print(landmark_preds, landmark_fname)
         return {"pred": landmark_preds, "filename": landmark_fname}
 
-    else: 
+    else:
         return {}
+
+
+@app.route('/quickdraw', methods=['GET', 'POST'])
+def quickdraw_predict():
+    quickdraw_animal_map = ['ant', 'bat', 'bear', 'bee', 'bird', 'butterfly', 'camel', 'cat', 'cow', 'dog', 'dolphin', 'dragon', 'duck', 'elephant', 'fish', 'flamingo', 'frog', 'giraffe', 'hedgehog', 'horse', 'kangaroo', 'lion',
+                            'lobster', 'mermaid', 'monkey', 'mosquito', 'mouse', 'octopus', 'owl', 'panda', 'penguin', 'pig', 'rabbit', 'raccoon', 'shark', 'sheep', 'snail', 'snake', 'spider', 'squirrel', 'teddy-bear', 'tiger', 'whale', 'zebra']
+
+    if request.method == 'POST':
+        quick_draw = request.form['url']
+        quick_draw = quick_draw[init_Base64:]
+        quick_draw_decoded = base64.b64decode(quick_draw)
+
+        # Fix later(to PIL version)
+        # Conver bytes array to PIL Image
+        # imageStream = io.BytesIO(draw_decoded)
+        # img = Image.open(imageStream)
+
+        quick_img = np.asarray(bytearray(quick_draw_decoded), dtype="uint8")
+        quick_img = cv2.imdecode(quick_img, cv2.IMREAD_GRAYSCALE)
+        quick_img = cv2.resize(quick_img, (28, 28),
+                               interpolation=cv2.INTER_AREA)
+        quick_img = Image.fromarray(quick_img)
+
+        weight_path = './weight/quickdraw_90_animal.pth'
+        quick_model = LeNet(num_classes=44).to(device)
+        quick_img, quick_pred = quickdraw_evaluation(
+            quick_img, weight_path, quick_model)
+
+        quick_pred = int(quick_pred)
+        quick_label = quickdraw_animal_map[quick_pred]
+    return render_template('draw_quickdraw.html')
 
 
 if __name__ == '__main__':
--- src/imageClassification/pad.js
+++ src/imageClassification/pad.js
@@ -1,37 +1,54 @@
 import React, { Component } from 'react'
 import SignaturePad from 'react-signature-canvas'
-import {Button} from 'semantic-ui-react';
+import { Button } from 'semantic-ui-react';
+import axios from "axios";
 
 import styles from './styles.module.css'
 
 class Pad extends Component {
-  state = {trimmedDataURL: null}
+  state = { value: null }
   sigPad = {}
   clear = () => {
     this.sigPad.clear()
   }
-  trim = () => {
-    this.setState({trimmedDataURL: this.sigPad.getTrimmedCanvas()
-      .toDataURL('image/png')})
-  }
-  render () {
-    let {trimmedDataURL} = this.state
+
+  fileUploadHandler = () => {
+    this.setState({
+      value: this.sigPad.getTrimmedCanvas()
+        .toDataURL('image/png')
+    })
+    console.log(this.state.value);
+    const fd = new FormData()
+    fd.append('url', this.state.value)
+    axios
+      .post("http://localhost:9000/mnist_pad", fd, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  };
+
+  render() {
+    let { value } = this.state
     return <div className={styles.container}>
       <div className={styles.sigContainer}>
-        <SignaturePad canvasProps={{className: styles.sigPad}}
+        <SignaturePad maxWidth="50" canvasProps={{ className: styles.sigPad }}
           ref={(ref) => { this.sigPad = ref }} />
       </div>
       <div>
-	<Button.Group>
-        <Button onClick={this.clear}>  Clear    </Button>
-	<Button.Or />  
-        <Button positive onClick={this.trim}> Submit </Button></Button.Group>
+        <Button.Group>
+          <Button onClick={this.clear}>  Clear    </Button>
+          <Button.Or />
+          <Button positive onClick={this.fileUploadHandler}> Submit </Button></Button.Group>
       </div>
-      {trimmedDataURL
+      {value
         ? <img className={styles.sigImage}
-          src={trimmedDataURL} />
+          src={value} />
         : null}
-      
+
     </div>
   }
 }
